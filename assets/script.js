3e4c07daa60636faf56c829429ca04e65a4bbd25
vojkovic <brockv@tuta.io>
2023-03-01 07:33:48 +0800

    Remove jQuery as a Dependency.

--- assets/script.js
+++ assets/script.js
@@ -1,75 +1,68 @@
-// Regex Search
-$(document).ready(function () {
-      var wordsArray = arrayData;
-     
-      $("[name='username']").keyup(function () {
-        var words = $(this).val().replace(/_/g, '.');
-        
-          console.log("Solving for:",words);
-          try {
-            var searchWord = words.toLowerCase()
+const wordsArray = arrayData;
+const result = document.querySelector("#result");
 
-            var reg = new RegExp(searchWord);
-            $("#result").html('');
-            for (var j = 0; j < wordsArray.length; j++) {
-              var value = wordsArray[j];
-              var eachWord = value.toLowerCase()
-
-              if (reg.test(eachWord)) {
-
-                var i;
-                if (searchWord.length != eachWord.length)
-                  continue;
-                for (i = 0; i < searchWord.length; i++) {
-                  if (searchWord[i] == '.')
-                    continue;
-                  if (searchWord[i] != eachWord[i])
-                    break;
-
-                }
-                if (i == searchWord.length)
-                  $("#result").append(value + '<br>' + '<br>');
-              }
-            }
+function handleKeyUp(event, username) {
+  const words = event.target.value.replace(/_/g, '.').toLowerCase();
+  try {
+    const reg = new RegExp(words.replace(/_./g, '[^.\s]'));
+    result.innerHTML = '';
+    for (j = 0; j < wordsArray.length; j++) {
+      const value = wordsArray[j].toLowerCase();
+      if (reg.test(value) && words.length === value.length) {
+        let isMatch = true;
+        for (i = 0; i < words.length; i++) {
+          if (words[i] == '.') continue;
+          if (words[i] != value[i]) {
+            isMatch = false;
+            break;
           }
-          catch (e) {
-            // regular expression error
-          }
-        
-      });
-    });  
+        }
+        if (isMatch) {
+          result.innerHTML += wordsArray[j] + '<br><br>';
+        }
+      }
+    }
+  } catch (e) {
+    console.error(e); // Log any regular expression errors
+  }
+}
+
+document.querySelector("[name='username']").addEventListener("keyup", function (event) {
+  handleKeyUp(event, this);
+});
 
 // Usage Modal
-  var usageModal = document.getElementById("usage-modal");
-  var usageBtn = document.getElementById("usage-btn");
-  var usageCloseBtn = document.getElementById("usage-modal-close");
+const usageModal = document.getElementById("usage-modal");
+const usageBtn = document.getElementById("usage-btn");
+const usageCloseBtn = document.getElementById("usage-modal-close");
 
-  usageBtn.addEventListener("click", function() {
-    usageModal.style.display = "block";
-    aboutModal.style.display = "none";
-  });
-  usageCloseBtn.addEventListener("click", function() {
-    usageModal.style.display = "none";
-  });
+usageBtn.addEventListener("click", function () {
+  usageModal.style.display = "block";
+  aboutModal.style.display = "none";
+});
+
+usageCloseBtn.addEventListener("click", function () {
+  usageModal.style.display = "none";
+});
 
 // About Modal
-  var aboutModal = document.getElementById("about-modal");
-  var aboutBtn = document.getElementById("about-btn");
-  var aboutCloseBtn = document.getElementById("about-modal-close");
+const aboutModal = document.getElementById("about-modal");
+const aboutBtn = document.getElementById("about-btn");
+const aboutCloseBtn = document.getElementById("about-modal-close");
 
-  aboutBtn.addEventListener("click", function() {
-    aboutModal.style.display = "block";
-    usageModal.style.display = "none";
-  });
-  aboutCloseBtn.addEventListener("click", function() {
-    aboutModal.style.display = "none";
-  });
+aboutBtn.addEventListener("click", function () {
+  aboutModal.style.display = "block";
+  usageModal.style.display = "none";
+});
 
+aboutCloseBtn.addEventListener("click", function () {
+  aboutModal.style.display = "none";
+});
 
 // Character Count
-var count = document.getElementById("count")
-var input = document.getElementById("input")
+const count = document.getElementById("count");
+const input = document.getElementById("input");
 
-input.addEventListener("input", function(e){
-  count.innerHTML = e.target.value.length;
-})
+input.addEventListener("input", function (event) {
+  count.innerHTML = event.target.value.length;
+});
